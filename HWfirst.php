96a5e5bcb5f881a77300cb4d9d423e82841c5bce
Yura <bobr515yura@yandex.ru>
2025-04-17 23:34:27 +0300

    HW

--- HWfirst.php
+++ HWfirst.php
@@ -1,12 +1,24 @@
 <?php
-echo "Имя файла".__FILE__;
-echo " Строка".__LINE__;
 
-$a='Рыба';
-$b='человек';
+echo "Любые введенные числа с плавющей точкой, будут округлены в меньшую сторону!\n";
 
-echo <<<END
+fscanf(STDIN, "%d\n", $stdin1) && is_int($stdin1) ?: fwrite(STDERR, "Введите пожалуйста число") && exit;
+fscanf(STDIN, "%d\n", $stdin2) && is_int($stdin1) ?: fwrite(STDERR, "Введите пожалуйста число") && exit;
+$stdin = [$stdin1, $stdin2];
 
-$a рыбою сыта, а $b человеком
-END;
+if ($stdin2===0) {
+    fwrite(STDERR, "На ноль делить нельзя!");
+    exit;
+}
+
+$result = [];
+
+foreach ($stdin as $el) {
+        array_push($result, $el);
+} 
+
+$stdout = $result[0] / $result[1];
+
+fwrite(STDOUT, "Ответ: $stdout");
+
 ?>
