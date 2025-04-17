10a8d7af76ea795eb8f59c3f13c359c75dc099cb
Yura <bobr515yura@yandex.ru>
2025-04-17 23:41:38 +0300

    HW

--- HWfirst.php
+++ HWfirst.php
@@ -3,7 +3,7 @@
 echo "Любые введенные числа с плавющей точкой, будут округлены в меньшую сторону!\n";
 
 fscanf(STDIN, "%d\n", $stdin1) && is_int($stdin1) ?: fwrite(STDERR, "Введите пожалуйста число") && exit;
-fscanf(STDIN, "%d\n", $stdin2) && is_int($stdin1) ?: fwrite(STDERR, "Введите пожалуйста число") && exit;
+fscanf(STDIN, "%d\n", $stdin2) && is_int($stdin2) ?: fwrite(STDERR, "Введите пожалуйста число") && exit;
 $stdin = [$stdin1, $stdin2];
 
 if ($stdin2===0) {
